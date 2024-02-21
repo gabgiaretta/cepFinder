8330ffec5b53e50c24a1b304d8cda515fc140a92
Gabriel Giaretta <gabrielpaulukgiaretta@gmail.com>
2024-02-21 10:14:47 -0300

    implementação da API e tartamento de exceções

--- script.js
+++ script.js
@@ -1,19 +1,6 @@
-const myobject = {
-  cep: "85902-120",
-  logradouro: "Rua Crissiumal",
-  complemento: "até 2399/2400",
-  bairro: "Jardim La Salle",
-  localidade: "Toledo",
-  uf: "PR",
-  ibge: "4127700",
-  gia: "",
-  ddd: "45",
-  siafi: "7927",
-};
-
 document.addEventListener("DOMContentLoaded", function () {
   let informationDiv = document.getElementById("information");
-
+  let myobject = JSON.parse(localStorage.getItem("data"));
   if (informationDiv) {
     let content = `
       CEP: ${myobject.cep}<br>
@@ -29,11 +16,43 @@ document.addEventListener("DOMContentLoaded", function () {
   }
 });
 
-function findCep(cepValue) {
-  window.location = "/resultado.html";
-  console.log(cepValue);
+function findCep() {
+  let cepValue = document.getElementById("inputValue").value;
+  fetchCep(cepValue);
 }
 
 function goBack() {
+  localStorage.clear();
   window.location = "/index.html";
+}
+
+function fetchCep(cepValue) {
+  if (cepValue.length != 8) {
+    alert("CEP inválido");
+    throw new Error(`O cep não possui 8 caractéres`);
+  }
+  let url = `https://viacep.com.br/ws/${cepValue}/json/`;
+  fetch(url)
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error(`Falha na solicitação`);
+      }
+      return response.json();
+    })
+    .then((data) => {
+      if (data.cep == undefined) {
+        alert("CEP inválido");
+        throw new Error(`Cep invalido`);
+      }
+      let datainfo = JSON.stringify(data);
+      localStorage.setItem("data", datainfo);
+      if (localStorage.Error) {
+        return;
+      }
+      window.location = "/resultado.html";
+    })
+    .catch((error) => {
+      console.error(`Erro na solicitação: ${error.message}`);
+      return;
+    });
 }
